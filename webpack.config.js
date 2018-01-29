960ef5e230769e98aadee9cc27b6aa115456e5f1
Kyle Uehlein <kyleuehlein@gmail.com>
2018-01-29 13:28:49 -0500

    add background image, revert webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
 'use strict'
 
-const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
-
 module.exports = {
   entry: './client/app.jsx',
   output: {
@@ -21,48 +17,18 @@ module.exports = {
         options: {
           presets: ['react', 'es2015'],
         }
-      },
-      {
+      }, {
         test: /\.scss$/,
-        include: [
-          path.resolve(__dirname, 'client/styles')
-        ],
-        use: ExtractTextPlugin.extract({
-          publicPath: '../../',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            }, {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
-      },
-      {
-        test: /\.png$/,
+
         use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'client/assets/[name].[ext]'
-            }
-          }
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
       }
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.scss', '.css', '.png', '*']
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: 'client/styles/app.css'
-    })
-  ]
+    extensions: ['.js', '.jsx', '*']
+  }
 }
