364708795f77ce7241a02f339d00151c6afaf803
Kyle Uehlein <kyleuehlein@gmail.com>
2018-02-06 23:44:34 -0500

    feat: make home page image dynamic, fix about box

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -6,17 +6,20 @@ import me from '../assets/self1.png'
 
 export default function Home() {
   return (
-    <div>
+    <div className='position-main image-shadow'>
       <img
         src={ me }
         alt='this is a picture of me holding a chihuahua'
-        className='image-shadow position-main'
       />
       <div className='text-over-picture'>
         <h3>Writer, musician, kind of weird,</h3>
         <h3>but in a good way.</h3>
-        <h5>and developer of course</h5>
+        <h5>(and developer of course)</h5>
       </div>
     </div>
   )
 }
+
+// do not squash image
+// make text move based on resize
+// WORK ON ABOUT NEXT
