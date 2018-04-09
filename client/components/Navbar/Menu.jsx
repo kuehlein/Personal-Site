e7fe3a6ce8d255435c8369210dcd736ea7e70754
Kyle Uehlein <kyleuehlein@gmail.com>
2018-04-09 00:52:21 -0400

    feat: implement basic menu transition for mobile

--- client/components/Navbar/Menu.jsx
+++ client/components/Navbar/Menu.jsx
@@ -2,11 +2,13 @@
 
 import React from 'react'
 
-import fish from '../../assets/francisco-goya-fish.png'
+import fish1 from '../../assets/francisco-goya-fish.png'
+import fish2 from '../../assets/small-fish.png'
 
 
 export default function Menu(props) {
   const visibilityClass = `${props.visible ? 'visible' : 'invisible'}`
+  const fish = screen.width > 321 ? fish1 : fish2
 
   return (
     <div
