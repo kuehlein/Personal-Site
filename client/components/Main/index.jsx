d68693a8f796fe5936c509f5b9db4009e0f414b0
Kyle Uehlein <kyleuehlein@gmail.com>
2018-05-23 16:02:09 -0400

    feat: center resume / remove name on resume

--- client/components/Main/index.jsx
+++ client/components/Main/index.jsx
@@ -11,20 +11,23 @@ export default function Main(props) {
     <div id="main-container"
     className="mobile-frame">
       <Navbar />
-      <div className="name-container">
-        <h1 className="name-position">K</h1>
-        <h1 className="name-position">y</h1>
-        <h1 className="name-position">l</h1>
-        <h1 className="name-position">e</h1>
-        <h1 className="name-position"><br /></h1>
-        <h1 className="name-position">U</h1>
-        <h1 className="name-position">e</h1>
-        <h1 className="name-position">h</h1>
-        <h1 className="name-position">l</h1>
-        <h1 className="name-position">e</h1>
-        <h1 className="name-position">i</h1>
-        <h1 className="name-position">n</h1>
-      </div>
+      {
+        window.location.pathname !== "/resume" &&
+        <div className="name-container">
+          <h1 className="name-position">K</h1>
+          <h1 className="name-position">y</h1>
+          <h1 className="name-position">l</h1>
+          <h1 className="name-position">e</h1>
+          <h1 className="name-position"><br /></h1>
+          <h1 className="name-position">U</h1>
+          <h1 className="name-position">e</h1>
+          <h1 className="name-position">h</h1>
+          <h1 className="name-position">l</h1>
+          <h1 className="name-position">e</h1>
+          <h1 className="name-position">i</h1>
+          <h1 className="name-position">n</h1>
+        </div>
+      }
       <div className="main-child">
         { props.children }
       </div>
